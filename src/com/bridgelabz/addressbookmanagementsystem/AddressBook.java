277dcbf31fa28a317749b77a7a73cecdf6bf32b2
Chandan <chandanmohanty11111>
2021-10-25 20:19:58 +0530

    ADDED:Person and addressbook details

---
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookmanagementsystem;
+import java.util.ArrayList;
+
+public class AddressBook{
+
+    ArrayList <ContactPerson> contactList = new ArrayList<ContactPerson>();
+}
