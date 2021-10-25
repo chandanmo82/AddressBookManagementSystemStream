76f0d89f9a6e72290d68bb7ef368c84c22aa2bd9
Chandan <chandanmohanty11111>
2021-10-25 21:13:40 +0530

    REFACTORED:Method to count people by city or state

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookDirectoryIF.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookDirectoryIF.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.addressbookmanagementsystem;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,4 +11,5 @@ public interface AddressBookDirectoryIF {
     public void searchByCity();
     public void searchByState();
     public void displayPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
+    public void countPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
 }
